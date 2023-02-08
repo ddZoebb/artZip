4258afcbb13865e44e6373e42dd193d8a316c385
ej-rarus <lpl2001@naver.com>
2023-02-08 13:51:13 +0900

    ㅇㅇ

---
+++ src/test/java/com/artZip/demo/ArtZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artZip.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtZipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
