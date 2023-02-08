650cc1230a94422838234adb72a1f00e3a923e2a
ej-rarus <lpl2001@naver.com>
2023-02-08 13:59:25 +0900

    commit test

--- src/main/java/com/artZip/demo/ArtZipApplication.java
+++ src/main/java/com/artZip/demo/ArtZipApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArtZipApplication {
-// github commit test22
+// github commit test2222
 	public static void main(String[] args) {
 		SpringApplication.run(ArtZipApplication.class, args);
 	}
